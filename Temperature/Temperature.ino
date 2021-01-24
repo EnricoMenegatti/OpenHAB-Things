372d0a20f6584e9b1f2dac77a048d388eca937da
Enrico Menegatti <enricomenegatti13@gmail.com>
2021-01-24 14:21:02 +0100

    rimossa gestione relay.

--- Temperature/Temperature.ino
+++ Temperature/Temperature.ino
@@ -5,8 +5,6 @@
 
 //ESP----------------------------------------------------------------------------------------------------------------
 bool resetESP = false, allSetup = false, serverAP = false;
-bool saveInputPinState, saveOutputPinState;
-int outputPin, inputPin;
 double thisTime, lastTime;
 
 //WI-FI----------------------------------------------------------------------------------------------------------------
@@ -16,10 +14,9 @@ char password[40] = "Menegatti13";//Menegatti13
 
 //MQTT----------------------------------------------------------------------------------------------------------------
 const char* mqtt_server = "localhost";
-const char* sub_topic = "home/esp8266/relay_command";
-const char* pub_topic = "home/esp8266/relay_status";
+const char* sub_topic = "home/esp8266/temperature_req";
+const char* pub_topic = "home/esp8266/temperature";
 const int port = 1883;
-
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -44,14 +41,7 @@ void setup()
   MQTT_Setup();
   
 // I/O
-  inputPin = 16;//D0 default
-  outputPin = 5;//D1 default
-
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(inputPin, INPUT);
-  pinMode(outputPin, OUTPUT);
-  
-  saveInputPinState = digitalRead(inputPin);
 }
 
 //MAIN---------------------------------------------------------------------------------------------------------------------
@@ -59,26 +49,6 @@ void loop()
 {
 //verify mqtt broker connection
   if (!client.connected()) reconnect();
-  
-  if(saveInputPinState != digitalRead(inputPin)) //on toggle
-  {
-    if(saveOutputPinState == true) 
-    {
-      digitalWrite(LED_BUILTIN, HIGH);
-      digitalWrite(outputPin, LOW);
-      saveOutputPinState = false;
-    }
-    else
-    {        
-      digitalWrite(LED_BUILTIN, LOW);
-      digitalWrite(outputPin, HIGH);
-      saveOutputPinState = true;
-    }
-    
-    saveInputPinState = digitalRead(inputPin);
-    Serial.print("INPUT toggle to: "); Serial.println(saveInputPinState);
-    Serial.print("OUTPUT toggle to: "); Serial.println(saveOutputPinState);
-  }
 
   ArduinoOTA.handle();
   client.loop();
